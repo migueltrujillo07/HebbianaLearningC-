fd7a13c50e82b52ab02d0b4b03fdf87e05a8ab31
Miguel Trujillo López <99068687+migueltrujillo07@users.noreply.github.com>
2024-03-19 23:49:01 -0600

    Update hebbianLearning.cpp
    Update Changes

--- hebbianLearning.cpp
+++ hebbianLearning.cpp
@@ -5,15 +5,30 @@ int main()
 	//Indicar el tamaño de la matriz de entrada x
 	int ne = 2;
 
-	NeuronaHebbiana nh(ne);
+	//Creacion de la clase Neurona Hebbiana
+	NeuronaHebbiana<int> nh(ne);
 	nh.ingresoValores();
 	nh.visualizar();
 	nh.Entrenar();
-	printf("El resultado del entrenamiento es:\n");
+	printf("El resultado del entrenamiento es (Hebbiana):\n");
 	nh.Imprimir();
 
-	////Evaluando el entrenamiento
-	printf("\n\nEvaluando el resultado:\n\n");
+	////Evaluando el entrenamiento Hebbiana
+	printf("\n\nEvaluando el resultado (Hebbiana): \n\n");
 	nh.Evaluar();
+
+
+	//Creacion de la neurona Perceptron
+	NeuronaPerceptron<int> Np(ne);
+	Np.ingresoValores();
+	Np.visualizar();
+	Np.Entrenar();
+	printf("El resultado del entrenamiento es (Perceptron) :\n");
+	Np.Imprimir();
+
+	////Evaluando el entrenamiento Perceptron
+	printf("\n\nEvaluando el resultado (Perceptron):\n\n");
+	Np.Evaluar();
+
 	return 0;
 }
